bad0d1879f4d5633f148d3d78338eea34c2592ad
Jussi Judin <jjudin+github@iki.fi>
2018-07-11 01:01:08 +0300

    Copy unique backtraces into a directory

--- process-crashes.sh
+++ process-crashes.sh
@@ -25,5 +25,10 @@ echo >&2 "Running $AFL_ID with $PYTHON_CMD"
 
 while read -r filename; do
     "$PYTHON_CMD" "$TARGET_SCRIPT" "$filename" && continue
+    stack_trace_sha256=$(
+        "$PYTHON_CMD" "$TARGET_SCRIPT" "$filename" 1>/dev/null 2>"$OUTDIR"/current-crash) || :
     echo >&2 "$PYTHON_CMD" "$TARGET_SCRIPT" "$filename"
+    read -r stack_trace_sum _ <<< "$(sha256sum -b "$OUTDIR"/current-crash)"
+    mkdir -p "$TARGET"/crashes-raw
+    cp "$filename" "$TARGET"/crashes-raw/"$stack_trace_sum".in
 done <<< "$(find "$OUTDIR" -type f | grep crashes/id | sort)"
