062280ff3b5fd2da741777ca9cfec21597a93e83
Jussi Judin <jjudin+github@iki.fi>
2018-07-10 23:38:14 +0300

    Add py-afl-tmin wrapper

---
+++ py-afl-tmin-input.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+TARGET=$1
+FILENAME=$2
+PYTHON_CMD=${PYTHON_CMD:-python3}
+
+TARGET_SCRIPT=$TARGET/fuzz.py
+if ! [[ -f "$TARGET_SCRIPT" ]]; then
+    echo >&2 "Fuzz target $TARGET/fuzz.py does not exist!"
+    exit 1
+fi
+
+BASE=$(basename "$FILENAME")
+mkdir -p "$TARGET"/crashes
+OUTNAME=$TARGET/crashes/$BASE
+
+exec py-afl-tmin -i "$FILENAME" -o "$OUTNAME" -- \
+     "$PYTHON_CMD" "$TARGET_SCRIPT" @@
