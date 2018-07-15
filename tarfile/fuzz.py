957614f06c30acb4ced5da7626d321745dba9c42
Jussi Judin <jjudin+github@iki.fi>
2018-07-15 12:34:18 +0300

    Add tarfile module

---
+++ tarfile/fuzz.py
@@ -0,0 +1,22 @@
+import afl
+import sys
+import tarfile
+
+
+def fuzz():
+    with open(sys.argv[1], "rb") as fp:
+        try:
+            with tarfile.open(mode="r:", fileobj=fp) as tp:
+                members = tp.getmembers()
+                list(tp.getnames())
+                for member in members:
+                    member.tobuf()
+                    tp.extractfile(member)
+                for name in tp.getnames():
+                    pass
+        except tarfile.TarError:
+            pass
+
+
+while afl.loop():
+    fuzz()
