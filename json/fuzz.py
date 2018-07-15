63f6303aa888adec80666a2b1626737b3588964c
Jussi Judin <jjudin+github@iki.fi>
2018-07-16 00:59:20 +0300

    Test JSON scanstring implementation equivalence

--- json/fuzz.py
+++ json/fuzz.py
@@ -1,16 +1,23 @@
+#!/usr/bin/env python3
+
 import afl
 import json
 import json.decoder
 import sys
 
-# Enforce Python implementation of the scanstring function:
-json.decoder.scanstring = json.decoder.py_scanstring
+orig_scanstring = json.decoder.scanstring
 
 while afl.loop():
     try:
-        data = json.load(open(sys.argv[1], errors="surrogateescape"))
+        data_default = json.load(open(sys.argv[1], errors="surrogateescape"))
+        # Enforce Python implementation of the scanstring function:
+        json.decoder.scanstring = json.decoder.py_scanstring
+        data_python = json.load(open(sys.argv[1], errors="surrogateescape"))
+        assert data_default == data_python
     except RecursionError:
         continue
     except json.decoder.JSONDecodeError:
         continue
-    json.dumps(data)
+    finally:
+        json.decoder.scanstring = orig_scanstring
+    json.dumps(data_default)
