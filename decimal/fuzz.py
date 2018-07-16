f9bde7de7bfcca6375b100535571a471e23d0e87
Jussi Judin <jjudin+github@iki.fi>
2018-07-16 19:10:15 +0300

    Check that C and Python versions of decimal module are equivalent

--- decimal/fuzz.py
+++ decimal/fuzz.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import afl
-import _pydecimal as decimal
+import _pydecimal as decimal_py
+import _decimal as decimal_c
 import math
 import sys
 
@@ -10,78 +11,79 @@ def fuzz():
     with open(sys.argv[1], errors="surrogateescape") as fp:
         data = fp.read()
     try:
-        d = decimal.Decimal(data)
+        dp = decimal_py.Decimal(data)
+        dc = decimal_c.Decimal(data)
     except ValueError:
         return
     except ZeroDivisionError:
         return
-    except decimal.InvalidOperation:
+    except (decimal_py.InvalidOperation, decimal_c.InvalidOperation):
         return
-    repr(d)
-    d.is_canonical()
-    d.is_finite()
-    d.is_infinite()
-    d.is_nan()
-    d.is_normal()
-    d.is_snan()
-    d.is_qnan()
-    d.is_signed()
-    d.is_subnormal()
-    d.is_zero()
+    assert repr(dp) == repr(dc)
+    assert dp.is_canonical() == dc.is_canonical()
+    assert dp.is_finite() == dc.is_finite()
+    assert dp.is_infinite() == dc.is_infinite()
+    assert dp.is_nan() == dc.is_nan()
+    assert dp.is_normal() == dc.is_normal()
+    assert dp.is_snan() == dc.is_snan()
+    assert dp.is_qnan() == dc.is_qnan()
+    assert dp.is_signed() == dc.is_signed()
+    assert dp.is_subnormal() == dc.is_subnormal()
+    assert dp.is_zero() == dc.is_zero()
     try:
-        d + d
-        d - d
-        d * d
-        d.adjusted()
-        d.as_tuple()
-        d.canonical()
-        d.compare(d)
-        d.conjugate()
-        d.copy_abs()
-        d.copy_negate()
-        d.copy_sign(d)
-        d.exp()
-        d.fma(1, 1)
-        d.ln()
-        d.log10()
-        d.logb()
-        d.logical_and(d)
-        d.logical_invert()
-        d.logical_or(d)
-        d.logical_xor(d)
-        d.max(d)
-        d.max_mag(d)
-        d.min(d)
-        d.min_mag(d)
-        d.next_minus()
-        d.next_plus()
-        d.next_toward(d)
-        d.normalize()
-        d.number_class()
-        d.quantize(d)
-        d.radix()
-        d.remainder_near(d)
-        d.rotate(d)
-        d.same_quantum(d)
-        d.scaleb(d)
-        d.shift(d)
-        d.sqrt()
-        d.to_eng_string()
-        d.to_integral()
-        d.to_integral_exact()
-        d.to_integral_value()
+        assert repr(dp + dp) == repr(dc + dc)
+        assert repr(dp - dp) == repr(dc - dc)
+        assert repr(dp * dp) == repr(dc * dc)
+        assert repr(dp.adjusted()) == repr(dc.adjusted())
+        assert repr(dp.as_tuple()) == repr(dc.as_tuple())
+        assert repr(dp.canonical()) == repr(dc.canonical())
+        assert repr(dp.compare(dp)) == repr(dc.compare(dc))
+        assert repr(dp.conjugate()) == repr(dc.conjugate())
+        assert repr(dp.copy_abs()) == repr(dc.copy_abs())
+        assert repr(dp.copy_negate()) == repr(dc.copy_negate())
+        assert repr(dp.copy_sign(dp)) == repr(dc.copy_sign(dc))
+        assert repr(dp.exp()) == repr(dc.exp())
+        assert repr(dp.fma(1, 1)) == repr(dc.fma(1, 1))
+        assert repr(dp.ln()) == repr(dc.ln())
+        assert repr(dp.log10()) == repr(dc.log10())
+        assert repr(dp.logb()) == repr(dc.logb())
+        assert repr(dp.logical_and(dp)) == repr(dc.logical_and(dc))
+        assert repr(dp.logical_invert()) == repr(dc.logical_invert())
+        assert repr(dp.logical_or(dp)) == repr(dc.logical_or(dc))
+        assert repr(dp.logical_xor(dp)) == repr(dc.logical_xor(dc))
+        assert repr(dp.max(dp)) == repr(dc.max(dc))
+        assert repr(dp.max_mag(dp)) == repr(dc.max_mag(dc))
+        assert repr(dp.min(dp)) == repr(dc.min(dc))
+        assert repr(dp.min_mag(dp)) == repr(dc.min_mag(dc))
+        assert repr(dp.next_minus()) == repr(dc.next_minus())
+        assert repr(dp.next_plus()) == repr(dc.next_plus())
+        assert repr(dp.next_toward(dp)) == repr(dc.next_toward(dc))
+        assert repr(dp.normalize()) == repr(dc.normalize())
+        assert repr(dp.number_class()) == repr(dc.number_class())
+        assert repr(dp.quantize(dp)) == repr(dc.quantize(dc))
+        assert repr(dp.radix()) == repr(dc.radix())
+        assert repr(dp.remainder_near(dp)) == repr(dc.remainder_near(dc))
+        assert repr(dp.rotate(dp)) == repr(dc.rotate(dc))
+        assert repr(dp.same_quantum(dp)) == repr(dc.same_quantum(dc))
+        assert repr(dp.scaleb(dp)) == repr(dc.scaleb(dc))
+        assert repr(dp.shift(dp)) == repr(dc.shift(dc))
+        assert repr(dp.sqrt()) == repr(dc.sqrt())
+        assert repr(dp.to_eng_string()) == repr(dc.to_eng_string())
+        assert repr(dp.to_integral()) == repr(dc.to_integral())
+        assert repr(dp.to_integral_exact()) == repr(dc.to_integral_exact())
+        assert repr(dp.to_integral_value()) == repr(dc.to_integral_value())
         try:
-            d.as_integer_ratio()
-            round(d, 1)
-            float(d)
-            int(d)
-            math.floor(d)
-            math.ceil(d)
+            assert repr(dp.as_integer_ratio()) == repr(dc.as_integer_ratio())
+            assert repr(round(dp, 1)) == repr(round(dc, 1))
+            assert repr(float(dp)) == repr(float(dc))
+            assert repr(int(dp)) == repr(int(dc))
+            assert repr(math.floor(dp)) == repr(math.floor(dc))
+            assert repr(math.ceil(dp)) == repr(math.ceil(dc))
         except OverflowError:
             pass
         except ValueError:
             pass
-    except decimal.DecimalException:
+    except decimal_py.DecimalException:
         pass
 
 
