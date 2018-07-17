325734c5f151858c10b50125e2eb783a100f4440
Jussi Judin <jjudin+github@iki.fi>
2018-07-17 17:50:39 +0300

    Check encoding results for json module

--- json/fuzz.py
+++ json/fuzz.py
@@ -5,19 +5,45 @@ import json
 import json.decoder
 import sys
 
+orig_make_scanner = json.scanner.make_scanner
 orig_scanstring = json.decoder.scanstring
+orig_c_make_encoder = json.encoder.c_make_encoder
+orig_encode_basestring_ascii = json.encoder.encode_basestring_ascii
+orig_encode_basestring = json.encoder.encode_basestring
 
-while afl.loop():
+
+def fuzz():
     try:
         data_default = json.load(open(sys.argv[1], errors="surrogateescape"))
         # Enforce Python implementation of the scanstring function:
+        json.scanner.make_scanner = json.scanner.py_make_scanner
         json.decoder.scanstring = json.decoder.py_scanstring
         data_python = json.load(open(sys.argv[1], errors="surrogateescape"))
-        assert data_default == data_python
     except RecursionError:
-        continue
+        return
     except json.decoder.JSONDecodeError:
-        continue
+        return
     finally:
+        json.scanner.make_scanner = orig_make_scanner
         json.decoder.scanstring = orig_scanstring
-    json.dumps(data_default)
+
+    try:
+        # Make sure that we don't compare different floating point
+        # values that are never equal, as float("nan") == float("nan")
+        # test results always in false value.
+        json.dumps(data_default, allow_nan=False)
+    except ValueError:
+        return
+
+    assert data_default == data_python
+    json.encoder.c_make_encoder = None
+    json.encoder.encode_basestring_ascii = json.encoder.py_encode_basestring_ascii
+    json.encoder.encode_basestring = json.encoder.py_encode_basestring
+    json.dumps(data_default, allow_nan=False)
+    json.encoder.c_make_encoder = orig_c_make_encoder
+    json.encoder.encode_basestring_ascii = orig_encode_basestring_ascii
+    json.encoder.encode_basestring = orig_encode_basestring
+
+
+while afl.loop():
+    fuzz()
